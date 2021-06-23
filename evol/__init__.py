dce08ea62868ffa6ccfa73d32c755a7bb0c9620a
tjacek <tjacek@agh.edu.pl>
2021-06-23 15:07:44 +0200

    Validation class

--- evol/__init__.py
+++ evol/__init__.py
@@ -4,17 +4,20 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import ens,exp,learn,files
 
 class EvolEnsemble(ens.Ensemble):
-    def __init__(self,read=None,transform=None):
+    def __init__(self,valid=None,read=None,transform=None):
         super(EvolEnsemble,self).__init__(read,transform)
+        if(valid is None):
+            valid=Validation()	
+        self.valid=valid
 
     def __call__(self,paths,clf="LR",s_clf=None):
         votes,datasets=super(EvolEnsemble,self).make_votes(paths,clf)
-        validation_votes(datasets,clf)
-#        weights=selfind_weights(datasets)
+        weights=self.find_weights(datasets)
         result=votes.weighted(weights)
         return result
 
-#    def find_weights(self,datasets,clf="LR"):
+    def find_weights(self,datasets,clf="LR"):
+        results=self.valid(datasets,clf)
 #        loss_fun=self.loss(results)
 #        bound_w = [(0.01, 1.0)  for _ in results]
 #        init_matrix=np.ones((6, len(bound_w)))
@@ -24,6 +27,20 @@ class EvolEnsemble(ens.Ensemble):
 #        loss_fun.iter=0
 #        weights=result['x']
 #        return weights
+
+class Validation(object):
+    def __init__(self,selector_gen=None):
+        if( selector_gen is None):
+            selector_gen=StratGen()	
+        self.selector_gen=selector_gen	
+
+    def __call__(self,datasets,clf="LR"):
+        train=[data_i.split()[0] for data_i in datasets]
+        names=list(train[0].keys())
+        selector= self.selector_gen(names)
+        results= [learn.train_model(train_i,clf_type=clf,selector=selector)
+                for train_i in train]
+        return results
 
 class StratGen(object):
     def __init__(self, n_split=2,test_size=0.5):
@@ -35,17 +52,7 @@ class StratGen(object):
         y=[name_i.get_cat() for name_i in names]
         for train_index, test_index in self.sss.split(names,y):
             train_names=[ names[i] for i in train_index]
-            raise Exception(train_names)
-            return files.SetSelector(train_names)
-
-def validation_votes(datasets,clf="LR"):
-    train=[data_i.split()[0] for data_i in datasets]
-    names=list(train[0].keys())
-    sss=StratGen()
-    selector=sss(names)
-    results= [learn.train_model(train_i,clf_type=clf,selector=selector)
-                for train_i in train]
-    return results
+            return files.SetSelector(train_names)  
 
 if __name__ == "__main__":
     dataset=".."
